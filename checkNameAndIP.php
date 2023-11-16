0b37565d4f271e72215a10d019d3b8436d866e11
Tymon Jastrzębski <125652292+HeyTymon@users.noreply.github.com>
2023-11-16 22:43:50 +0100

    checkNameAndIP is working, echo clean up in sendMeasure

--- checkNameAndIP.php
+++ checkNameAndIP.php
@@ -4,30 +4,6 @@
 
     $connection = mysqli_connect($host, $user, $password, $dbName);
 
-    function sendDataToESP($state) {
-        $url = "http://" . $_POST['ip'] . "/sendState"; 
-
-        //echo ($state) ? $url : "Error";
-
-        //Check this 
-        $data = array('state' => $state);
-        $options = array(
-            'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($data),
-            ),
-        );
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-
-        if ($result !== false) {
-            echo "Text sent to ESP32: " . $text;
-        } else {
-            echo "Error sending text to ESP32";
-        }
-    }
-
 
     if($connection->connect_errno != 0) {
         echo "Error: ".$connection->connect_errno;
@@ -40,20 +16,18 @@
             if($result = $connection->query($sqlQuery)) {
                 if ($result->num_rows > 0) {
                     $row = $result->fetch_assoc();
-                    echo "Sensor " . $row['name'] . " was found!<br>";
+                    echo "present";
                     $isSensor = true;
                 } else {
-                    echo "Sensor " . $_POST['name'] . " was not found!<br>";
+                    echo "not present";
                     $isSensor = false;
                 }
             }
 
         } else {
-            echo "<br> No data";
+            echo "No data";
         }  
     }
-
-    sendDataToESP($isSensor);
 
     mysqli_close($connection);
 ?>
--- sendMeasure.php
+++ sendMeasure.php
@@ -6,20 +6,19 @@
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     } else {
-        echo "Connected successfully<br>";
 
         if (isset($_POST["temperature"]) && isset($_POST["humidity"]) && isset($_POST["sensorName"])) {
 
             $sql = "INSERT INTO `measurementstoday`(`temperature`, `humidity`, `sensorName`) VALUES (" . $_POST["temperature"] . "," . $_POST["humidity"] . ",'" . $_POST["sensorName"] . "')";
 
             if (mysqli_query($conn, $sql)) {
-                echo "New record created successfully<br>";
+                echo "New record created successfully";
             } else {
                 echo "Error: " . $sql . " " . mysqli_error($conn);
             }
             
         } else {
-            echo "<br>No data";
+            echo "No data";
         }
 
         mysqli_close($conn);
