371a5864f396d6b3873d6e40118596d1339a9f3d
Tymon Jastrzębski <125652292+HeyTymon@users.noreply.github.com>
2023-11-30 15:32:27 +0100

    Checks if cluster does exists

--- newSensor.php
+++ newSensor.php
@@ -19,6 +19,16 @@
         $sqlQuery1 = "SELECT * FROM `sensors` WHERE ip = '$newSensorIP' OR name = '$newSensorName'";
 
         $sqlQuery2 = "INSERT INTO `sensors` (`id`, `name`, `ip`, `cluster`) VALUES (NULL, '$newSensorName', '$newSensorIP', '$clusterSensor')";
+
+        $sqlQuery3 = "SELECT * FROM `clusters` WHERE `id` = '$clusterSensor'";
+
+        $result = @$connection->query($sqlQuery3);
+        if($result->num_rows == 0) {
+            $_SESSION['newSensorSession'] = "Cluster does not exists";
+            header('Location: settings.php');
+            $connection->close();
+            exit();
+        }
 		
 		if($result = @$connection->query($sqlQuery1)) {
             if($result->num_rows == 0) {
